a941321cdd98ec3525454b5226af12568b201a1e
wujianyunhaer <472323588@qq.com>
2018-06-29 23:58:45 +0800

    6.29

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import fastClick from 'fastclick'
+import './registerServiceWorker'
+import 'assets/reset.css'
+import 'assets/border.css'
+import 'assets/iconfont.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
