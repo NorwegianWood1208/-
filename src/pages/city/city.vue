f9793be2527e35eef7dc1f13857a541b6df92c24
wujianyunhaer <472323588@qq.com>
2018-07-03 17:58:15 +0800

    city

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,14 +1,23 @@
 <template>
     <div>
         <city-head></city-head>
+        <city-seach></city-seach>
+        <city-list></city-list>
+        <city-nav></city-nav>
   </div>
 </template>
 <script>
 import CityHead from "./components/header";
+import CitySeach from "./components/seach";
+import CityList from "./components/list";
+import CityNav from "./components/nav";
 export default {
   name: "City",
   components: {
-    CityHead
+    CityHead,
+    CitySeach,
+    CityList,
+    CityNav
   }
 };
 </script>
